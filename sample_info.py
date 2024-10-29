33e8488a54ed845f4e25e6f5bd3fdc950207424a
AJAYBOTS <channelallin0@gmail.com>
2024-10-29 11:20:42 +0530

    Create sample_info.py

---
+++ sample_info.py
@@ -0,0 +1,22 @@
+# Bot information
+SESSION = 'Media_search'
+USER_SESSION = 'User_Bot'
+API_ID = 12345
+API_HASH = '0123456789abcdef0123456789abcdef'
+BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
+USERBOT_STRING_SESSION = ''
+
+# Bot settings
+CACHE_TIME = 300
+USE_CAPTION_FILTER = False
+
+# Admins, Channels & Users
+ADMINS = [12345789, 98765432]  # Admin user IDs (numeric)
+CHANNELS = [-10012345678, -100987654321]  # Channel IDs (numeric)
+AUTH_USERS = []
+AUTH_CHANNEL = None
+
+# MongoDB information
+DATABASE_URI = "mongodb://username:password@host1:port1,...,hostN/?retryWrites=true&w=majority"
+DATABASE_NAME = 'Telegram'
+COLLECTION_NAME = 'channel_files'  # Use a unique name for each bot if needed
